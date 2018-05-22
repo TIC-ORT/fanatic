43f69b9a73d805c125adea7ed3e396de5033b337
Andres Moritan <amoritan@me.com>
2018-05-22 09:56:02 -0300

    Style dependencies and SCSS support

--- src/App.vue
+++ src/App.vue
@@ -11,44 +11,16 @@
 </template>
 
 <script>
+import 'normalize.css'
 export default {
   name: 'app'
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700');
+
 body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
+  font-family: $main-font;
 }
 </style>
