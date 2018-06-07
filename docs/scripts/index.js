3b7ffd60cd315f8418e1e7a1cfe70915f439776c
Andres Moritan <amoritan@me.com>
2018-06-06 21:27:03 -0300

    Landing page

---
+++ docs/scripts/index.js
@@ -0,0 +1,13 @@
+// var modeIconsSettings = {
+//   pathTimingFunction: Vivus.EASE
+// };
+// function addFill (e) {
+//   if (e.el.id === 'trainingModeIcon') {
+//     document.getElementById('trainingModeText').classList.add('fadeInUp');
+//   } else if (e.el.id === 'playModeIcon') {
+//     document.getElementById('playModeText').classList.add('fadeInDown');
+//   }
+//   // e.el.classList.add('filled');
+// }
+// var trainingModeIcon = new Vivus('trainingModeIcon', modeIconsSettings, addFill);
+// var playModeIcon = new Vivus('playModeIcon', modeIconsSettings, addFill);
